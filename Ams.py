465ca49d512e2624f6b6d390797249884580eb37
OBAIDULLAH786 <obaidullah@eng.rizvi.edu.in>
2026-09-07 11:05:30 +0530

    V1.0

---
+++ Ams.py
@@ -0,0 +1,7 @@
+# Airline Management System - Version 1.0
+# Feature: issue and cancel tickets
+def issue_ticket(ticket_id, passenger_id):
+    print("Ticket", ticket_id, "issued to passenger", passenger_id)
+def cancel_ticket(ticket_id):
+    print("Ticket", ticket_id, "cancelled")
+  
