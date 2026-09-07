9701b0c9b7cbf58afd0134491a7b32c0d7551abb
OBAIDULLAH786 <obaidullah@eng.rizvi.edu.in>
2026-09-07 11:10:29 +0530

    V1.1

--- Ams.py
+++ Ams.py
@@ -4,4 +4,7 @@ def issue_ticket(ticket_id, passenger_id):
     print("Ticket", ticket_id, "issued to passenger", passenger_id)
 def cancel_ticket(ticket_id):
     print("Ticket", ticket_id, "cancelled")
-  
+  def calculate_fine(days_late, rate=5):
+  fine = days_late * rate
+  print(&quot;Fine = Rs.&quot;, fine)
+  return fine
